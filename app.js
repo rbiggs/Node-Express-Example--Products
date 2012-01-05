48e670849d7a912248db13fa301a10242b179d4a
Robert Biggs <rbiggs@ymail.com>
2012-01-05 12:59:03 -0800

    Update for user authentication
    Added session support.
    Added user authentication. This is restricted to areas where the user can edit products, add new products or add new photos.
    Add the ability for a user to logout.

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var fs = require('fs');
+var express = require('express')
+  , routes = require('./routes')
+  , fs = require('fs')
+  , Mongoose = require('mongoose').Mongoose
+  , crypto = require('crypto');
 
 var app = module.exports = express.createServer();
 
@@ -17,7 +19,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
+  app.use(express.session({ secret: 'robot cowboy' }));
   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
@@ -31,23 +33,121 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
-// Routes
-
-app.get("/", function(req, res) {
-  //throw new Error("Here's my error!");
-  res.render("index");
+app.dynamicHelpers({
+  message: function(req){
+    var err = req.session.error
+      , msg = req.session.success;
+    delete req.session.error;
+    delete req.session.success;
+    if (err) return err;
+    if (msg) return msg;
+  }
 });
+
+//Generate a salt for the user to prevent rainbow table attacks.
+
+var users = {
+  rbiggs: {
+    name: 'rbiggs'
+    , salt: 'randomly-generated-salt'
+    , pass: hash('dingo', 'randomly-generated-salt')
+  }
+};
+// Used to generate a hash of the plain-text password + salt
+
+function hash(msg, key) {
+  return crypto.createHmac('sha256', key).update(msg).digest('hex');
+}
+
+// Authenticate using our plain-object database of doom!
+
+function authenticate(name, pass, fn) {
+  var user = users[name];
+  // Query the db for the given username.
+  if (!user) return fn(new Error('cannot find user'));
+  /* 
+     Apply the same algorithm to the POSTed password, applying
+     the hash against the pass / salt, if there is a match we
+     found the user. 
+  */
+  if (user.pass == hash(pass, user.salt)) return fn(null, user);
+  // Otherwise password is invalid
+  fn(new Error('invalid password'));
+}
+
+function restrict(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.session.error = 'Access denied!';
+    res.redirect('/login');
+  }
+}
+
+function accessLogger(req, res, next) {
+  console.log('/restricted accessed by %s', req.session.user.name);
+  next();
+}
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/login', function(req, res) {
+  if (req.session.user) {
+    req.session.success = 'Authenticated as ' + req.session.user.name
+    res.redirect('/products');
+  } else {
+    res.render('login');
+  }
+});
+
+app.post('/login', function(req, res){
+  authenticate(req.body.username, req.body.password, function(err, user){
+    if (user) {
+      // Regenerate session when signing in
+      // to prevent fixation 
+      req.session.regenerate(function(){
+        /*
+          Store the user's primary key 
+          in the session store to be retrieved,
+          or in this case the entire user object,
+        */
+        req.session.user = user;
+        res.redirect('back');
+      });
+    } else {
+      req.session.error = 'Authentication failed. Please check your username and password.';
+      res.redirect('back');
+    }
+  });
+});
+
+app.get('/logout', function(req, res){
+  // Destroy the user's session to log them out.
+  // Will be re-created next request.
+  req.session.destroy(function(){
+    res.redirect('/');
+  });
+});
+
+// Routes
 
 var products = require('./products');
 var photos = require('./photos');
 
+function getAllProducts(req, res) {  
+  res.render('products/index', {locals: {
+      products: products.all
+  }});
+}
 app.get('/products', function(req, res) {
-    res.render('products/index', {locals: {
-        products: products.all
-    }});
+  getAllProducts(req, res);
 });
 
-app.get('/products/new', function(req, res) {
+// For new products, restrict access to logged in user.
+
+app.get('/products/new', restrict, function(req, res) {
   var product = req.body && req.body.product || products.new();
   photos.list(function(err, photo_list) {
     if (err) {
@@ -68,12 +168,18 @@ app.post('/products', function(req, res) {
 
 app.get('/products/:id', function(req, res) {
     var product = products.find(req.params.id);
-    res.render('products/show', {
+    if (product) {
+      res.render('products/show', {
         locals: {product: product}
-    });
+      });
+    } else {
+      getAllProducts(req, res);
+    }
 });
 
-app.get('/products/:id/edit', function(req, res) {
+// For editing a product, restrict access to logged in user.
+
+app.get('/products/:id/edit', restrict, function(req, res) {
   var product = products.find(req.params.id);
   photos.list(function(err, photo_list) {
     if (err) {
@@ -103,7 +209,9 @@ app.get('/photos', function(req, res) {
   });
 });
 
-app.get('/photos/new', function(req, res) {
+// For new photos, restrict access to logged in user.
+
+app.get('/photos/new', restrict, function(req, res) {
   res.render('photos/new');
 });
 
@@ -119,5 +227,5 @@ app.post('/photos', function(req, res) {
     res.redirect('/photos');
   
 });
-app.listen(6060);
+app.listen(3333);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
